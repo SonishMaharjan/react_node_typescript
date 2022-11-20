cf43993d4a0f357f72846c4255e6baad838a9a22
sonish <sonishmzn95@gmail.com>
2022-11-20 16:03:53 +0545

    BE: Database Configured

---
+++ nurse_be/src/db/connect.ts
@@ -0,0 +1,28 @@
+import mongoose, { ConnectOptions } from "mongoose";
+import config from "config";
+import log from "../logger";
+
+function connect() {
+  const dbUri = config.get("dbUri") as string;
+
+  //   mongoose.connect(
+  //     mongoAtlasUri,
+  //     { useNewUrlParser: true, useUnifiedTopology: true },
+  //     () => console.log(" Mongoose is connected")
+  //   );
+
+  return mongoose
+    .connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions)
+    .then(() => {
+      log.info("Database connected successfully");
+    })
+    .catch((error) => {
+      log.error("db connection error", error);
+      process.exit(1);
+    });
+}
+
+export default connect;
