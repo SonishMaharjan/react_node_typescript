9a2657565a3d8c02e197a2c6eb33301fbc8c3586
sonish <sonishmzn95@gmail.com>
2022-12-05 18:50:24 +0545

    FE: React app setup

---
+++ nurse_fe/nurse_management/src/components/pages/loginPage.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const LoginPage: React.FC = () => {
+  //   const [term, setTerm] = useState("");
+
+  //  **Replace dispatch** const dispatch = useDispatch();
+  //   const { searchRepositories } = useActions(); // **Replaced * dispatch**
+
+  // ** Fixe Use Any **
+  //   const { data, error, loading } = useSelector(
+  //     (state: any) => state.repositoriesReducer
+  //   );
+  // ** Fixed Use Any **
+  //   const { data, error, loading } = useTypedSelector(
+  //     (state) => state.repositoriesReducer
+  //   );
+
+  //   console.log(data);
+
+  //   console.log(state);
+
+  //   const onSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //     event.preventDefault();
+
+  //     // **Replace dispatch** dispatch(actionCreators.searchRepositories(term) as any);
+  //     searchRepositories(term); //**Replaced * dispatch**
+  //   };
+
+  //   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //     setTerm(event.target.value);
+  //   };
+
+  return <div>Login page</div>;
+};
+
+export default LoginPage;
