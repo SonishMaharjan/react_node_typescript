367265bef3d49d9dbffd568fc0c714fa6f612335
sonish <sonishmzn95@gmail.com>
2022-12-19 07:36:29 +0545

    BE: Add test cases

--- nurse_be/src/test/jwt.utils.test.ts
+++ nurse_be/src/test/jwt.utils.test.ts
@@ -1,36 +1,19 @@
-import { sign, getPasswordHash } from "../utils/jwt.utils";
+import { compareString } from "../utils/jwt.utils";
 
-// test("Verify sign token", () => {
-//   // Arrange
-//   const userInput = {
-//     id: 131,
-//     name: "hi",
-//     email: "hi@world.com",
-//     created_at: "2022-12-11T16:53:33.818Z",
-//     updated_at: "2022-12-11T16:53:33.818Z",
-//     session: 45,
-//   };
+test("Verify password hash ", async () => {
+  // Arrange
+  const passwordInput = "Hello123";
+  const hashPasswordInput =
+    "$2b$10$QbvXAyKI6CN8tbhBuJOu8ehqyUtxhVWmuJYtsq1kMF75OI823i0xa";
+  const hashPasswordInput2 = `$2b$10$miRM2xC5cePQB2Fku2cRwe/zV.7qdZv/t52sYUUtJBt9/ypmPyB2m`;
 
-//   const optionsInput = { expiresIn: "1y" };
-//   const expectedOutput = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMxLCJuYW1lIjoiaGkiLCJlbWFpbCI6ImhpQHdvcmxkLmNvbSIsImNyZWF0ZWRfYXQiOiIyMDIyLTEyLTExVDE2OjUzOjMzLjgxOFoiLCJ1cGRhdGVkX2F0IjoiMjAyMi0xMi0xMVQxNjo1MzozMy44MThaIiwic2Vzc2lvbiI6NDYsImlhdCI6MTY3MTM2MTI0OSwiZXhwIjoxNzAyOTE4ODQ5fQ.VkbQ8424vPLoo4Px81dleSID2mwGMo3Pknio-qpj8ng`;
+  const expectedOutput = true;
 
-//   // Act
-//   const actualOutput = sign(userInput, optionsInput);
+  // Act
+  const actualOutput = await compareString(passwordInput, hashPasswordInput);
+  const actualOutput2 = await compareString(passwordInput, hashPasswordInput2);
 
-//   // Assert
-//   expect(actualOutput).toEqual(expectedOutput);
-// });
-
-// test("Verify password hash ", () => {
-//   // Arrange
-//   const passwordInput = "helloworld";
-
-//   const expectedOutput =
-//     "$2b$10$heX/OUE4xpYAOMglR2ublu8o2l0Hai0MPMQ2tGpS47j3W6N.vYQ2S";
-
-//   // Act
-//   const actualOutput = getPasswordHash(passwordInput);
-
-//   // Assert
-//   expect(actualOutput).toEqual(expectedOutput);
-// });
+  // Assert
+  expect(actualOutput).toEqual(expectedOutput);
+  expect(actualOutput2).toEqual(expectedOutput);
+});
